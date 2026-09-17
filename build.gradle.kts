d108fb6ea52040836256580d7ac0303a0936190e
Leksan-code <lekuspe@gmail.com>
2026-09-17 22:56:23 +0500

    Remove repository declarations from build.gradle.kts
    Removed repository declarations from buildscript and allprojects sections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,8 @@
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
     dependencies {
-        // Подключаем официальные стабильные плагины Android и Kotlin
+        // Подключаем инструменты сборки Android и Kotlin
         classpath("com.android.tools.build:gradle:8.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
     }
 }
 
