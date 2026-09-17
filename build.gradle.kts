b3495ab7b716a393a0b9e800f11cf37fbc43855a
Leksan-code <lekuspe@gmail.com>
2026-09-17 23:06:51 +0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-buildscript {
-    dependencies {
-        // Подключаем официальные стабильные плагины Android и Kotlin
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
+plugins {
+    id("com.android.application") version "8.4.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 tasks.register("clean", Delete::class) {
