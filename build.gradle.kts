4e1ce9f4288c35d9ee0a5ba344b97f333f810d1d
Leksan-code <lekuspe@gmail.com>
2026-09-17 23:02:21 +0500

    Update comments for clarity in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     dependencies {
-        // Подключаем официальные инструменты сборки Android и Kotlin
+        // Подключаем официальные стабильные плагины Android и Kotlin
         classpath("com.android.tools.build:gradle:8.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
